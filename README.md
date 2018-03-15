fd1d6f6ab2bfa3ef9af570e30bce0a542d523147
Franklin Webber <franklin.webber@gmail.com>
2018-03-15 00:12:28 -0500

    Adds initial commit for tomcat profile
    This is a conversion from a control file to a profile.
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tomcat Compliance Profile
+
+This ensures that Tomcat is correctly configured and securely running on your system.
---
+++ controls/example.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+# copyright: 2018, Franklin Webber
+
+title 'Tomcat Correctness'
+
+describe host('localhost', port: 8080, protocol: 'tcp') do
+  it { should be_reachable }
+end
+
+describe port(8080) do
+  it { should be_listening }
+  its('protocols') { should cmp('tcp') }
+  its('addresses') { should cmp('::') }
+end
+
+describe http('http://localhost:8080') do
+  its('body') { should match /Tomcat/ }
+end
+
+describe service('tomcat') do
+  it { should(be_running) }
+end
+
+# you add controls here
+# control 'tmp-1.0' do                        # A unique ID for this control
+#   impact 0.7                                # The criticality, if this control fails.
+#   title 'Create /tmp directory'             # A human-readable title
+#   desc 'An optional description...'
+#   describe file('/tmp') do                  # The actual test
+#     it { should be_directory }
+#   end
+# end
